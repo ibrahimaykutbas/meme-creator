899a2e14bf47bbd4b4e71fb3f85993b474bfc0a8
İbrahim Aykut BAŞ <ibrahimaykutbas@gmail.com>
2024-09-17 18:29:58 +0300

    Meme Creator

---
+++ MemeCreator/MemeCreatorApp.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+@main
+struct MemeCreatorApp: App {
+    @StateObject private var fetcher = PandaCollectionFetcher()
+    
+    var body: some Scene {
+        WindowGroup {
+            NavigationStack {
+                MemeCreator()
+                    .environmentObject(fetcher)
+            }
+            .navigationViewStyle(.stack)
+        }
+    }
+}
